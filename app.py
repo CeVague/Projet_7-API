7e1bb1d34571d125ff47988f55c49be52e99d888
CeVague <cedric.vachaudez@netc.fr>
2023-07-06 20:32:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return jsonify({"message": "hello world"})
+
+@app.route('/api', methods=['GET'])
+def search():
+    args = request.args
+    return jsonify(args)
 
 if __name__ == '__main__':
     app.run()
