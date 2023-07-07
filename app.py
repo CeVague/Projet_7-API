f1ff3599f7574a9e89c5c80faabef3eb0147fedf
CeVague <cedric.vachaudez@netc.fr>
2023-07-07 18:15:39 +0200

    Ajout de shap waterfall

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
+import io
 import pickle
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
+import shap
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 
 import pandas as pd
 
@@ -20,18 +26,45 @@ with open('./data/columns.pkl', 'rb') as f:
 def index():
     return jsonify({"message": "hello world"})
 
-@app.route('/api', methods=['GET'])
-def get_client_shap():
-    client_line = data = request.json['data']
+def explain_client(client_line):
     client_line = pd.read_json(client_line, typ='series')
     
     client_line_scaled = scaler.transform([client_line[list(columns)]])
     client_line_explained = explainer(client_line_scaled)
     
+    return client_line_explained
+
+@app.route('/dataframe', methods=['GET'])
+def get_client_dataframe():
+    client_line = request.json['data']
+    client_line_explained = explain_client(client_line)
+    
     tmp = pd.DataFrame(client_line_explained.values, columns=columns, index=['shap']).T
     tmp['abs'] = tmp['shap'].abs()
     tmp = tmp.sort_values('abs', ascending=False)
     return tmp.to_json()
+
+@app.route('/plot/<forme>', methods=['GET'])
+def get_client_plot(forme):
+    client_line = request.json['data']
+    client_line_explained = explain_client(client_line)
+    
+    # Créer le graphique avec Matplotlib
+    plt.figure()
+    shap.plots.waterfall(client_line_explained[0], max_display=10, show=False)
+    fig = plt.gcf()
+    plt.xticks([])
+    plt.xlabel("<"+"-"*20+" "*10+"Accepté"+" "*40+"Refusé"+" "*10+"-"*20+">")
+    fig.set_size_inches(14, 6)
+    fig.tight_layout()
+
+    # Sauvegarder le graphique en tant que fichier image dans une mémoire tampon
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    # Renvoyer le contenu de la mémoire tampon comme réponse HTTP avec le type de contenu approprié
+    return Response(buffer.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run()
